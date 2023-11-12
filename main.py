bf86377380fce393cbf5effc00e31ffbe291cd7b
PABLO <105730702+PABLO123s@users.noreply.github.com>
2023-11-12 14:28:47 +0300

    Python l;asdas90

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print("hello world!")
+from PyQt5.QtWidgets import QApplication
+
+app = QApplication([])
+
+from main_window import *
+
+
+def switch_box():
+    if btn_next.text() == "Відповісти":
+        RadioGroupBox.hide()
+        AnswerGroupBox.show()
+        btn_next.setText("Наступне питання")
+
+    elif btn_next.text() == "Наступне питання":
+        RadioGroupBox.show()
+        AnswerGroupBox.hide()
+        btn_next.setText("Відповісти")
+
+btn_next.clicked.connect(switch_box)
+
+
+
+window.show()
+app.exec()
